972abad14014b832428afe6a30797862300e0bca
Paul Rudkovsky <p.rudkovsky@cherrit.com>
2013-03-31 17:38:29 +0300

    styling

--- sites/all/themes/zen/page.tpl.php
+++ sites/all/themes/zen/page.tpl.php
@@ -92,7 +92,7 @@
       </hgroup><!-- /#name-and-slogan -->
     <?php endif; ?>
 
-    <?php if ($secondary_menu): ?>
+   <!-- <?php if ($secondary_menu): ?>
       <nav id="secondary-menu" role="navigation">
         <?php print theme('links__system_secondary_menu', array(
           'links' => $secondary_menu,
@@ -107,7 +107,7 @@
         )); ?>
       </nav>
     <?php endif; ?>
-
+-->
     <?php print render($page['header']); ?>
 
   </header>
