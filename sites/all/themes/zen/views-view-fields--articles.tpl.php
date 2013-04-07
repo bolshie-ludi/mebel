09e6f12c4a6a596ccb9066a022c1df42345c88cc
Paul <bolshie.ludi@gmail.com>
2013-04-07 17:27:51 +0300

    views-articles

--- sites/all/themes/zen/views-view-fields--articles.tpl.php
+++ sites/all/themes/zen/views-view-fields--articles.tpl.php
@@ -1,12 +1,5 @@
-<?php foreach ($fields as $id => $field): ?>
-  <?php if (!empty($field->separator)): ?>
-    <?php print $field->separator; ?>
-  <?php endif; ?>
+<article>
+<?php print $fields['title']->content;
+print $fields['body']->content;?>
 
-  <article>
-  <header>
-  <h3><a href=""><?php print $field->label_html; ?> </a></h3>
-    <span><?php print $field->content; ?> </span>
-  </header>
-  </article>
-<?php endforeach; ?>
+</article>
