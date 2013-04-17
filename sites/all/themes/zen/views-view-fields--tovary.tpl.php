09c18dbc789f6f81c8fc6241d3dd364f155a2127
Paul Rudkovsky <p.rudkovsky@cherrit.com>
2013-04-17 17:10:11 +0300

    footer logo work

--- sites/all/themes/zen/views-view-fields--tovary.tpl.php
+++ sites/all/themes/zen/views-view-fields--tovary.tpl.php
@@ -5,7 +5,11 @@
 	if (!empty($fields['field_price_2'])){
 	print '<div class="price">Цена: <del>'.$fields['field_price']->content.'</del><font color="#900"> '.$fields['field_price_2']->content.'</font></div>';
 	} else {
-		print '<div class="price">Цена: '.$fields['field_price']->content.'</div>';
+		if (!empty($fields['field_price'])){
+			print '<div class="price">Цена: '.$fields['field_price']->content.'</div>';
+		} else {
+			print '<div class="price">Только на заказ</div>';
+		}
 	}
 	 ?>
 </div>
